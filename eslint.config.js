2995de347b039cd489498c0576d07cef194c39c9
Alexander Arkhangelskiy <aarkhan@ya.ru>
2024-04-29 09:20:03 +0300

    Add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,24 @@
+import js from '@eslint/js';
+import globals from 'globals';
 import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
-  ...eslintPluginAstro.configs['flat/recommended'],
-  {
-    ignores: ["**/temp.js", "dist/*"]
-  },
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-    }
-  }
+	js.configs.recommended,
+	...eslintPluginAstro.configs['flat/recommended'],
+	eslintConfigPrettier,
+	{
+		languageOptions: {
+			globals: globals.browser,
+		},
+	},
+	{
+		ignores: ['**/temp.js', 'dist/*'],
+	},
+	{
+		rules: {
+			// override/add rules settings here, such as:
+			// "astro/no-set-html-directive": "error"
+		},
+	},
 ];
